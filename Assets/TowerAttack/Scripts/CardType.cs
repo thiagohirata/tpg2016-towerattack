6eff06ff25e7a06fc8819ed0083d2028c852b9f7
Thiago Hirata <hirata@devcase.com.br>
2016-09-23 19:33:16 -0300

    CardType

---
+++ Assets/TowerAttack/Scripts/CardType.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu(fileName = "Card", menuName = "TowerAttack/New Card")]
+public class CardType : ScriptableObject {
+    /// <summary>
+    /// Nome da carta
+    /// </summary>
+    public string cardName;
+    /// <summary>
+    /// Custo da carta para comprar
+    /// </summary>
+    public int cost;
+    /// <summary>
+    /// Custo da carta para usar
+    /// </summary>
+    public int manaCost;
+    /// <summary>
+    /// Prefab gerado ao jogar a carta
+    /// </summary>
+    public GameObject playPrefab;
+    /// <summary>
+    /// Descrição da carta
+    /// </summary>
+    public string cardDescription;
+}
