5e8473d0690a006b7f1343480852de7c6833011c
Thiago Hirata <hirata@devcase.com.br>
2016-09-23 22:15:09 -0300

    Compra de cartas

--- Assets/TowerAttack/Scripts/UI/BuyCardPanel.cs
+++ Assets/TowerAttack/Scripts/UI/BuyCardPanel.cs
@@ -4,6 +4,7 @@ using System.Collections;
 
 public class BuyCardPanel : MonoBehaviour {
     public CardType cardType;
+    Button buyButton;
 
     void Start () {
 	    if(cardType != null)
@@ -12,6 +13,25 @@ public class BuyCardPanel : MonoBehaviour {
             this.transform.FindChild("CardName").GetComponent<Text>().text = cardType.cardName;
             this.transform.FindChild("CardCost").GetComponent<Text>().text = "$" + cardType.cost;
             this.transform.FindChild("CardDescription").GetComponent<Text>().text = cardType.cardDescription;
+        }
+
+        buyButton = this.transform.FindChild("BuyButton").GetComponent<Button>();
+        buyButton.onClick.AddListener(() => BuyCard());
+    }
+
+    void Update()
+    {
+        PlayerData player = PlayerData.main;
+        buyButton.interactable = player.money >= cardType.cost;
+    }
+
+    public void BuyCard()
+    {
+        PlayerData player = PlayerData.main;
+        if(player.money >= cardType.cost)
+        {
+            player.money -= cardType.cost;
+            player.deck.Add(cardType);
         }
     }
 	
